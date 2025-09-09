efff096de494989113528f864041347e30fe54fe
dev-phoenix <dev-phoenix@mail.ru>
2025-09-09 21:10:50 +0300

    back router users

--- lara11_01/lara11/routes/api.php
+++ lara11_01/lara11/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+// app/Http/Resources/UserResource.php
+
+Route::get('/users', function() {
+    return UserResource::collection(User::all());
 });
